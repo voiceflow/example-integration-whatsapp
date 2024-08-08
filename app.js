e319b711c7a986e2e605d189b87504095b4360b6
schwasa <102740850+schwasa@users.noreply.github.com>
2024-08-08 13:23:08 +0200

    Update

--- app.js
+++ app.js
@@ -245,7 +245,6 @@ async function interact(user_id, request, phone_number_id, user_name) {
     },
   })
 
-
   // Sandro #1 new to nlu_protection post call parallel to other
   try {
     await axios({
@@ -264,10 +263,9 @@ async function interact(user_id, request, phone_number_id, user_name) {
   console.error('Error during POST to /variables request:', error);
   }
 
-
-  // Sandro um "last_conversation" zu Aktualisieren
-  const now = new Date();
-  const formattedDate = now.toISOString();
+  // Sandro um "last_conversation"
+  const rightNow = new Date();
+  const formattedDate = rightNow.toISOString();
   // first, check if user entry is existing
   try {
     const responseTracker = await axios({
@@ -326,28 +324,24 @@ async function interact(user_id, request, phone_number_id, user_name) {
   // } catch (error) {
   //   console.error('Error during POST to /interact: request', error);
   // }
-  try {
-    let response = await axios({
-        method: 'POST',
-        url: `${NLU_PROTECTION_URL}/interact`,
-        headers: {
-          Authorization: VF_API_KEY,
-          'Content-Type': 'application/json',
-          versionID: VF_VERSION_ID,
-          sessionID: session
-        },
-        data: {
-          user_id: user_id,
-          user_name: user_name,
-          session: session,
-          action: request,
-          config: DMconfig,
-        },
-      });
-    console.log('response:', response);
-    } catch (error) {
-        console.error('Error during POST to /interact: request', error);
-  }
+  let response = await axios({
+      method: 'POST',
+      url: `${NLU_PROTECTION_URL}/interact`,
+      headers: {
+        Authorization: VF_API_KEY,
+        'Content-Type': 'application/json',
+        versionID: VF_VERSION_ID,
+        sessionID: session
+      },
+      data: {
+        user_id: user_id,
+        user_name: user_name,
+        session: session,
+        action: request,
+        config: DMconfig,
+      },
+    });
+  console.log('response:', response);
 
 // existing code from VF
 //   let response = await axios({
